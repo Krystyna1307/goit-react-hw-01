8f5df4aad30a6b914bf20f159bbcc0f4683f6563
Krystyna1307 <kristina.dudnik8@gmail.com>
2024-10-29 09:20:36 +0100

    Social network profile without CSS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,15 @@
 import Profile from "./Profile/Profile";
+import userData from "../userData.json";
 
 const App = () => {
   return (
-    <>
-      <Profile />
-    </>
+    <Profile
+      name={userData.username}
+      tag={userData.tag}
+      location={userData.location}
+      image={userData.avatar}
+      stats={userData.stats}
+    />
   );
 };
 
